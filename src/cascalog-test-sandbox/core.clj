b90e7454d20c1a96d0d32d7e9d4f4624deff4051
phosphene <ed@cronos.net>
2014-03-18 21:54:51 -0400

    fix testing

---
+++ src/cascalog-test-sandbox/core.clj
@@ -0,0 +1,6 @@
+(ns cascalog-test-sandbox.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/cascalog-test-sandbox/core_test.clj
@@ -0,0 +1,7 @@
+(ns cascalog-test-sandbox.core-test
+  (:require [clojure.test :refer :all]
+            [cascalog-test-sandbox.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
