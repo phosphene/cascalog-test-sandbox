7fd7f96804e3e3e63a002d917a05afdbead24b3e
phosphene <ed@cronos.net>
2014-03-19 10:38:13 -0400

    cleanup

--- project.clj
+++ project.clj
@@ -1,19 +1,21 @@
-(defproject cascalog_spec_sandbox "0.1.0-SNAPSHOT"
-  :description "spec testing of cascalog"
+(defproject cascalog-test-sandbox "0.1.0-SNAPSHOT"
+  :description "testing of cascalog"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"] [cascalog/cascalog-core "2.0.0"]]
   :profiles {:dev 
              {:dependencies 
-              [[speclj "2.5.0"] 
+              [ 
+               [midje "1.6.3"]
                [org.apache.hadoop/hadoop-core "1.1.2"]
-               [lein-midje "3.0.1"]
+               
                [cascalog/midje-cascalog "1.10.1"]] 
               }
              }
-  :plugins [[speclj "2.5.0"]]
-  :test-paths ["spec"]
+  :plugins [[lein-midje "3.1.1"]]
+  :test-paths ["test/"]
   :repositories {"conjars" "http://conjars.org/repo"}
   :jvm-opts ["-Xms768m" "-Xmx768m"]
+  
   )
--- test/cascalog-test-sandbox/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns cascalog-test-sandbox.core-test
-  (:require [clojure.test :refer :all]
-            [cascalog-test-sandbox.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
