20aa0da458214798b442669ee09b1b27cd78cfab
phosphene <ed@cronos.net>
2014-03-19 13:08:57 -0400

    library issues in test

--- test/cascalog_test_sandbox/core_test.clj
+++ test/cascalog_test_sandbox/core_test.clj
@@ -1,18 +1,13 @@
 (ns cascalog-test-sandbox.core-test
-  (:use midje.sweet)
-  (:use [cascalog-test-sandbox.core]))
+  
+  (:use [cascalog-test-sandbox.core]
+        [cascalog.api]
+        [midje sweet cascalog])
+)
 
-(println "You sbould expect to see three failures below.")
-
-(facts "about `first-element`"
-  (fact "it normally returns the first element"
-    (first-element [1 2 3] :default) => 1
-    (first-element '(1 2 3) :default) => 1)
-
-  ;; I'm a little unsure how Clojure types map onto the Lisp I'm used to.
-  (fact "default value is returned for empty sequences"
-    (first-element [] :default) => :default
-    (first-element '() :default) => :default
-    (first-element nil :default) => :default
-    (first-element (filter even? [1 3 5]) :default) => :default))
-
+  (fact "Query should return a single tuple containing
+           [most-popular-user, follower-count]."
+          (max-followers-query :path) => (produces [["richhickey" 2961]])
+          (provided
+            (mysubquery :path) => [["sritchie09" 180]
+                                         ["richhickey" 2961]]))
