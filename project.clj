596c6f17222c6649516b4292ffb09d86e7d99696
phosphene <ed@cronos.net>
2014-03-19 10:43:44 -0400

    return of speclj

--- project.clj
+++ project.clj
@@ -6,14 +6,13 @@
   :dependencies [[org.clojure/clojure "1.5.1"] [cascalog/cascalog-core "2.0.0"]]
   :profiles {:dev 
              {:dependencies 
-              [ 
+              [[speclj "2.5.0"] 
                [midje "1.6.3"]
                [org.apache.hadoop/hadoop-core "1.1.2"]
-               
                [cascalog/midje-cascalog "1.10.1"]] 
               }
              }
-  :plugins [[lein-midje "3.1.1"]]
+  :plugins [[lein-midje "3.1.1"] [speclj "2.5.0"]]
   :test-paths ["test/"]
   :repositories {"conjars" "http://conjars.org/repo"}
   :jvm-opts ["-Xms768m" "-Xmx768m"]
