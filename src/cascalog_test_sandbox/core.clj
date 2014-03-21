e21d78b876d35c3d5b589573b6e5118e9e5a3481
phosphene <ed@cronos.net>
2014-03-21 13:34:46 -0400

    more tests with more midje-cascalog idioms in place

--- src/cascalog_test_sandbox/core.clj
+++ src/cascalog_test_sandbox/core.clj
@@ -30,6 +30,18 @@
                        ["?user" "?follower-count"])]
     (c/first-n src 1 :sort ["?follower-count"] :reverse true)))
 
+;; from http://sritchie.github.io/2012/01/22/cascalog-testing-20/
 
+(def src
+  [[1 2] [1 3]
+   [3 4] [3 6]
+   [5 2] [5 9]])
 
-
+;; adds the values in each input tuple, sorts the output and returns
+;; 2-tuples of the first number and the sum. [1 2] becomes [1 3], for
+;; example.
+(def query
+  (<- [?x ?sum]
+      (src ?x ?y)
+      (:sort ?x)
+      (c/sum ?y :> ?sum)))
--- test/cascalog_test_sandbox/core_test.clj
+++ test/cascalog_test_sandbox/core_test.clj
@@ -11,3 +11,18 @@
           (provided
             (mysubquery :path) => [["sritchie09" 180]
                                          ["richhickey" 2961]]))
+(facts "query produces 2-tuples from src defined in core.clj"
+  query => (produces [[3 10] [1 5] [5 11]])  ;; true
+  query => (produces [[1 5] [3 10] [5 11]])) ;; true
+
+(fact
+  query =not=> (produces [["string!" 11] [1 5] [5 11]])) ;; true
+
+(fact
+  query =not=> (produces [["string!" 11] [1 5] [5 11]])) ;; true
+
+
+(facts
+  query => (produces-suffix [[5 11]])        ;; true
+  query => (produces-suffix [[5 11]] :info)) ;; true
+
