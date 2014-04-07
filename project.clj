3fd72ac8a681f877145388d79fe160175e2e8e17
phosphene <ed@cronos.net>
2014-04-07 11:42:49 -0400

    add mahout to project

--- project.clj
+++ project.clj
@@ -8,7 +8,10 @@
                  [cascalog/cascalog-more-taps "2.0.0"] 
                  [cascalog/cascalog-checkpoint "2.0.0"]
                  [org.apache.lucene/lucene-core "4.7.0"]
-                 [org.apache.lucene/lucene-analyzers-common "4.7.0"]]
+                 [org.apache.lucene/lucene-analyzers-common "4.7.0"]
+                 [org.apache.mahout/mahout-core "0.9"]
+                 [org.apache.mahout/mahout-math "0.9"]
+                 ]
   :profiles {:dev 
              {:dependencies 
               [[speclj "2.5.0"] 
@@ -21,6 +24,6 @@
   :plugins [[lein-midje "3.1.3"] [speclj "2.5.0"]]
   :test-paths ["test/"]
   :repositories {"conjars" "http://conjars.org/repo"}
-  :jvm-opts ["-Xms514m" "-Xmx768m"]
+  :jvm-opts ["-Xms512m" "-Xmx768m"]
   :main cascalog-test-sandbox.core
   )
